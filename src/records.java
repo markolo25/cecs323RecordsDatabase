e4ac2be5644d8578326a1914d8386ff7ae43ff6f
Mark Mendoza <markolo25@gmail.com>
2016-03-11 20:02:15 -0800

    initial commit

---
+++ src/records.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author mark
+ */
+public class records {
+    static String USER;
+    static String PASS;
+    static String DBNAME;
+    static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";
+    static String DB_URL = "jdbc:derby://localhost:1527/";
+    
+    
+    public static void main(String[] args) {
+        
+    
+    }
+    
+    
+}
